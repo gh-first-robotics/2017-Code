61f941e9a1bbb17a160e47b63ebe6b96cc624d20
Benjamin Kovacs <bach1099-btkamazonwebservices@yahoo.com>
2017-01-26 16:03:33 -0500

    Created new class VisionProcessing.

--- src/org/usfirst/frc/team5530/robot/Robot.java
+++ src/org/usfirst/frc/team5530/robot/Robot.java
@@ -13,6 +13,7 @@ import org.usfirst.frc.team5530.robot.system.RobotSystem;
 import org.usfirst.frc.team5530.robot.system.Scaler;
 import org.usfirst.frc.team5530.robot.system.Shooter;
 import org.usfirst.frc.team5530.robot.teleop.Operator;
+import org.usfirst.frc.team5530.robot.VisionProcessing;
 
 import edu.wpi.first.wpilibj.AnalogInput;
 import com.ctre.CANTalon;
@@ -22,7 +23,6 @@ import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.SampleRobot;
 import edu.wpi.first.wpilibj.Servo;
 import edu.wpi.first.wpilibj.Timer;
-import edu.wpi.first.wpilibj.networktables.NetworkTable;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj.vision.USBCamera;
@@ -47,14 +47,14 @@ import edu.wpi.first.wpilibj.vision.USBCamera;
 public class Robot extends SampleRobot {
 	private RobotSystem[] systems;
 
-	private USBCamera camera;
+	public USBCamera camera;
 	private Operator teleop;
 	
 	private static final AutoProgram[] autons = { new ChillyFries(), new Porticullis(), new LowBar(), new Other() };
-	private SendableChooser defenseChooser;
+	public SendableChooser defenseChooser;
 
 	private static final String[] positions = { "1", "2", "3", "4", "5" };
-	private SendableChooser positionChooser;
+	public SendableChooser positionChooser;
 	
 	@Override
 	public void robotInit() {
@@ -115,109 +115,11 @@ public class Robot extends SampleRobot {
 		start("Teleoperation");
 		while (isOperatorControl() && isEnabled()) {
 			teleop.tick();
-			printTargetInformation();
+			VisionProcessing.printTargetInformation();
 			sleep(5);
 		}
 	}
-	public static double bestWHratio = 1; //figure out what this is
-	//largest area is best???
-	public static int bestTargetIndex =-1;
-	private static int testIndex = 0;
-	public static int secondBestTargetIndex = 0;
-	double screenCenter = 160; //50
-	public static int bestTarget(double[] widths, double[] heights/*, double[] areas*/){
-		if (widths.length > 0){		
-			//bestTargetIndex=0;
-			testIndex = 0;
-			secondBestTargetIndex=0;
-			System.out.println("widths.length" + widths.length);
-			System.out.println("heights.length" + heights.length);
-			
-			for (int i=0; i<Math.min(widths.length, heights.length); i++){
-				System.out.println("i " + i);
-				System.out.println("widths.length in for loop"+widths.length);
-				System.out.println("bestTargetIndex " + bestTargetIndex);
-				System.out.println("widths[i] "+widths[i]);
-				System.out.println("widths[testIndex] "+widths[testIndex]);
-				System.out.println("heights[i] "+heights[i]);
-				System.out.println("heights[testIndex] "+heights[testIndex]);
-				if (Math.abs(widths[i]/heights[i] - bestWHratio)<Math.abs(widths[testIndex]/heights[testIndex] - bestWHratio)){
-					//secondBestTargetIndex=bestTargetIndex;
-					//bestTargetIndex=i;
-					secondBestTargetIndex=testIndex;
-					testIndex=i;
-				}
-				//add something to include area
-			}
-			bestTargetIndex = testIndex;
-			return bestTargetIndex;
-		}
-		else{
-			return -1;
-		}
-	}
-	public int getbestTargetIndex(){
-		return bestTargetIndex;
-	}
-	public static boolean targetsFound = false;
-	static double k = 21*40; //is this the correct number?
-	public static double distanceToTarget(double width){ //TODO: change to use height in calculations as well
-		return k/width;
-	}
-	public static double center0;
-	public static double distanceToTarget;
-	NetworkTable table = NetworkTable.getTable("GRIP/myContoursReport");
-	public void printTargetInformation(){		
-		double[] areas = table.getNumberArray("area", new double[0]);
-		double[] widths = table.getNumberArray("width", new double[0]);
-		double[] heights = table.getNumberArray("height", new double[0]);
-		double[] centerXs = table.getNumberArray("centerX", new double[0]);
-		double[] centerYs = table.getNumberArray("centerY", new double[0]);
-		double[] solidities = table.getNumberArray("solidity", new double[0]);
-		System.out.println(table.isConnected()); //prints true
-		System.out.println(areas.length); //prints number of targets found
-		System.out.println(widths.length);
-		System.out.println(heights.length);
-		if (widths.length > 1){
-			targetsFound=true;
-		}
-		else {
-			targetsFound=false;
-		}
-		if (widths.length > 0){
-			
-			System.out.println("index of best target: "+bestTarget(widths, heights));
-			System.out.println("width: "+widths[bestTargetIndex]);
-			table.putNumber("bestTargetIndex", bestTargetIndex);
-			distanceToTarget = distanceToTarget(widths[bestTargetIndex]);
-			System.out.println("distance to target: "+distanceToTarget(widths[bestTargetIndex]));
-			table.putNumber("indexOfBestTargetdistanceToTarget", distanceToTarget(widths[bestTargetIndex]));
-			System.out.println("centerxs.length" + centerXs.length);
-			System.out.println("best target index for centerx" + bestTargetIndex);
-			if (centerXs.length > bestTargetIndex){
-			center0= centerXs[bestTargetIndex] - screenCenter;
-			System.out.println("center X of best target: "+centerXs[bestTargetIndex]);
-
-			}
-					}
-		else{
-			targetsFound=false;
-		}
-	}
 	
-	@Override
-	public void disabled() {
-		start("Disabled");				
-		while (isDisabled()) {
-			printTargetInformation();
-			Timer.delay(.5);
-		}
-	}
-
-	@Override
-	public void test() {
-		start("Test");
-	}
 
 	@SuppressWarnings("unchecked")
 	public <T extends RobotSystem> T getSystem(Class<T> clazz) {
@@ -245,7 +147,19 @@ public class Robot extends SampleRobot {
 		}
 	}
 
-	private static void start(String mode) {
+	public static void start(String mode) {
 		System.out.println("#    " + mode + "    #");
+	}
+	@Override
+	public void test() {
+		start("Test");
+	}
+	@Override
+	public void disabled() {
+		start("Disabled");				
+		while (isDisabled()) {
+			VisionProcessing.printTargetInformation();
+			Timer.delay(.5);
+		}
 	}
 }
